e371e69eb71370b6e2d21f4d2e33b998a179b860
diugalde <dua.0695@gmail.com>
2014-12-02 17:59:57 -0600

    Prueba borrar todo.
    Probando todavia con MElcochete

--- prueba.py
+++
@@ -1,8 +0,0 @@
-
-def chiste():
-    print ("habia un chile tan malo pero tan malo, que se bajaba de la mata para pegarle a los chilitos... ZAAAAAAAAA!!!")
-
-Yoooooooooooooooooooooooooooo 
-soy
-el
-aventureeeeeerrooooooooo!
